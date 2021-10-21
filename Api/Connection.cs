ede3a764216ed0936532255574d860a798d42513
Anthony Chu <anthony@anthonychu.ca>
2021-10-21 16:36:36 -0700

    Plain HTML + .NET 6 In-proc Functions

--- Api/Connection.cs
+++ Api/Connection.cs
@@ -25,9 +25,7 @@ namespace Api
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = $"Hello from Azure Functions .NET version: {System.Environment.Version}";
 
             return new OkObjectResult(responseMessage);
         }
