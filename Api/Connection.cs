3ce620c4ad0096a34453728eb015217042b2ef3a
Anthony Chu <anthony@anthonychu.ca>
2021-10-21 17:09:13 -0700

    Plain HTML + .NET 6 In-proc Functions

--- Api/Connection.cs
+++ Api/Connection.cs
@@ -14,7 +14,7 @@ namespace Api
     {
         [FunctionName("Connection")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
