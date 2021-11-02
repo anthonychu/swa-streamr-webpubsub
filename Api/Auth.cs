f503099860a4de7838da75898ee1f1bc882211ec
Anthony Chu <anthony@anthonychu.ca>
2021-11-01 17:07:01 -0700

    Port StreamR from SignalR to Web PubSub (#1)

---
+++ Api/Auth.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Azure.Functions.Worker.Http;
+
+public static class StaticWebAppsAuth
+{
+    private class ClientPrincipal
+    {
+        public string IdentityProvider { get; set; }
+        public string UserId { get; set; }
+        public string UserDetails { get; set; }
+        public IEnumerable<string> UserRoles { get; set; }
+    }
+
+    public static ClaimsPrincipal ParseUserInfo(HttpRequestData req)
+    {
+        var principal = new ClientPrincipal();
+
+        KeyValuePair<string, IEnumerable<string>>? header = req.Headers.FirstOrDefault(h => h.Key == "x-ms-client-principal");
+        if (header == null)
+        {
+            return null;
+        }
+
+        try
+        {
+
+            var data = header.Value;
+            var decoded = Convert.FromBase64String(header.Value.Value.First());
+            var json = Encoding.UTF8.GetString(decoded);
+            principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if (!principal.UserRoles?.Any() ?? true)
+            {
+                return new ClaimsPrincipal();
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            return new ClaimsPrincipal(identity);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+}
---
+++ Client/Models/UserVideo.cs
@@ -0,0 +1,23 @@
+namespace ServerlessStreamR.Client.Models
+{
+    public class UserVideo
+    {
+        private DateTime lastUpdated = DateTime.Now;
+        public DateTime LastUpdated
+        { 
+            get => lastUpdated;
+        }
+
+        private Frame? frame;
+        public Frame? Frame
+        {
+            get { return frame; }
+            set
+            {
+                frame = value;
+                lastUpdated = DateTime.Now;
+            }
+        }
+        
+    }
+}
